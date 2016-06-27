bb8b3e5059c46d5311d5f8a189b4d6db50e645a3
byjuanamn <juan.antonio.martin@gmail.com>
2016-06-27 21:13:30 +0200

    add Label y textfield
    Añadimos también las constraints leading y baseline del label

--- ALExample7Xamarin/ViewController.cs
+++ ALExample7Xamarin/ViewController.cs
@@ -15,6 +15,55 @@ namespace ALExample7Xamarin
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+
+
+
+			var searchLabel = new UILabel();
+			searchLabel.TranslatesAutoresizingMaskIntoConstraints = false;
+			searchLabel.Text = "Search:";
+			View.AddSubview(searchLabel);
+
+			var textField = new UITextField();
+			textField.TranslatesAutoresizingMaskIntoConstraints = false;
+			textField.BorderStyle = UITextBorderStyle.RoundedRect;
+			View.AddSubview(textField);
+
+			/*
+				Ajustamos el leading de la etiqueta
+			*/
+
+			var contraintSearchLabelLeading = NSLayoutConstraint.Create(searchLabel,
+																		NSLayoutAttribute.Leading,
+																		NSLayoutRelation.Equal, 
+			                                                            View,
+																		NSLayoutAttribute.LeadingMargin,
+																		1.0f,
+																		0.0f);
+			contraintSearchLabelLeading.Active = true;
+
+			/*
+			NSLayoutConstraint(item: searchLabel,
+                           attribute: .Baseline,
+                           relatedBy: .Equal,
+                           toItem: textField,
+                           attribute: .Baseline,
+                           multiplier: 1.0,
+                           constant: 0.0)
+            .active = true
+
+			*/
+			NSLayoutConstraint.Create(searchLabel,
+									  NSLayoutAttribute.Baseline,
+									  NSLayoutRelation.Equal,
+									  textField,
+									  NSLayoutAttribute.Baseline,
+									  1.0f,
+									  0.0f).Active = true;
+			
+
+
+
+
 		}
 
 		public override void DidReceiveMemoryWarning()
